46e8168d26bda1a23dc7f56a3e613e383bfcc486
Sir-aji-josh <joshman.aji@gmail.com>
2023-08-28 18:22:08 -0700

    0-safe_print_list.py

--- 0x05-python-exceptions/103-python.c
+++ 0x05-python-exceptions/103-python.c
@@ -6,8 +6,7 @@
 
 #include <Python.h>
 
-
-   /* Call the print functions */
+   /* the print functions */
 void print_python_list(PyObject *p);
 void print_python_bytes(PyObject *p);
 void print_python_float(PyObject *p);
