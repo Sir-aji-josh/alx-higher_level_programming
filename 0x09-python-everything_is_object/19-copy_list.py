8ff4912b83c8367fadbaa43cabaaef3a9c493b55
Sir-aji-josh <joshman.aji@gmail.com>
2023-09-05 18:01:57 -0700

    19-copy_list.py

--- 0x09-python-everything_is_object/19-copy_list.py
+++ 0x09-python-everything_is_object/19-copy_list.py
@@ -1,6 +1,3 @@
 #!/usr/bin/python3
-def magic_string(n):
-    return "BestSchool" * n
-"""function with an integer value (n)"""
-    result = magic_string(3)
-    print(result)
+def copy_list(l):
+    return l[:]
