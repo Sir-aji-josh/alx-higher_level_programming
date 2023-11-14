87c32185ab6a1960c9f6c155530a7c61328b6eee
Sir-aji josh <joshman.aji@gmail.com>
2023-11-14 06:53:16 -0800

    task 5

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,9 +1,6 @@
 #!/usr/bin/node
-
-const num = parseInt(process.argv[2]);
-
-if (process.argv.length < 3 || isNaN(num)) {
+if (isNaN(process.argv[2]) || process.argv[2] === undefined) {
   console.log('Not a number');
 } else {
-  console.log('My number: '.concat(num));
+  console.log('My number:', parseInt(process.argv[2]));
 }
