6974c51a896e56a08bc9c9e62e77c4951564a474
Sir-aji-josh <joshman.aji@gmail.com>
2023-11-24 19:04:33 -0800

    task 5 - All cities by state.

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -1,19 +1,18 @@
 #!/usr/bin/python3
-""" Takes in the name of a state as an argument and lists all cities of that
-state, using the database hbtn_0e_4_usa. """
-
-from sys import argv
+"""  lists all states from the database hbtn_0e_0_usa """
 import MySQLdb
+import sys
+
 
 if __name__ == "__main__":
-    db = MySQLdb.connect(
-            host='localhost', port=3306,
-            user=argv[1], passwd=argv[2],
-            db=argv[3], charset='utf8')
+    db = MySQLdb.connect(host="localhost", user=sys.argv[1],
+                         passwd=sys.argv[2], db=sys.argv[3], port=3306)
     cur = db.cursor()
-    cur.execute("SELECT * FROM `cities` as `cur` \
-                INNER JOIN `states` as `st` \
-                ON `cur`.`state_id` = `st`.`id` \
-                ORDER BY `cur`.`id`")
-    print(", ".join([ct[2] for ct in cur.fetchall() if ct[4] == argv[4]]))
-
+    cur.execute("""SELECT cities.name FROM
+                cities INNER JOIN states ON states.id=cities.state_id
+                WHERE states.name=%s""", (sys.argv[4],))
+    rows = cur.fetchall()
+    tmp = list(row[0] for row in rows)
+    print(*tmp, sep=", ")
+    cur.close()
+    db.close()
