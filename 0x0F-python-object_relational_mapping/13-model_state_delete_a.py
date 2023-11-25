b399964997c2e71b02b92a5440696d4e963fe545
Sir-aji-josh <joshman.aji@gmail.com>
2023-11-24 18:32:20 -0800

    task 13 - Delete states

--- 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
+++ 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
-""" 
-prints the State object with the name passed as argument from the database
+""" prints the State object with the name passed as argument from the database
 """
 import sys
 from model_state import Base, State
 from sqlalchemy import (create_engine)
 from sqlalchemy.orm import sessionmaker
+
 
 if __name__ == "__main__":
     engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
@@ -16,4 +16,3 @@ if __name__ == "__main__":
     for instance in session.query(State).filter(State.name.like('%a%')):
         session.delete(instance)
     session.commit()
-
