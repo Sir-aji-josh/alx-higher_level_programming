55ce75f94298e18459f66d18ec69b5c7e2adcff1
Sir-aji-josh <joshman.aji@gmail.com>
2024-01-03 16:37:52 -0800

    task 0. Readme

---
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+const fs = require('fs');
+const filename = process.argv[2];
+
+fs.readFile(filename, 'utf-8', (err, content) => {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log(content);
+  }
+});
